7e5f21b4630c4156813ac3128b50326b9168b758
Mattias Håheim Johnsen <mattiahj@unit.no>
2021-03-12 01:03:29 +0100

    Apply validation.

--- metadata-transformer/src/main/java/no/unit/nva/metadata/service/TranslatorService.java
+++ metadata-transformer/src/main/java/no/unit/nva/metadata/service/TranslatorService.java
@@ -1,39 +1,48 @@
 package no.unit.nva.metadata.service;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import no.unit.nva.metadata.NoLowercaseDcPrefixFix;
+import no.unit.nva.metadata.NoLowercaseDcPrefixRule;
+import no.unit.nva.metadata.NoLowercaseDctermsPrefixFix;
+import no.unit.nva.metadata.NoLowercaseDctermsPrefixRule;
 import org.apache.any23.Any23;
 import org.apache.any23.extractor.ExtractionException;
+import org.apache.any23.extractor.html.TagSoupParser;
 import org.apache.any23.filter.IgnoreAccidentalRDFa;
 import org.apache.any23.filter.IgnoreTitlesOfEmptyDocuments;
 import org.apache.any23.source.DocumentSource;
 import org.apache.any23.source.HTTPDocumentSource;
+import org.apache.any23.validator.DefaultValidator;
+import org.apache.any23.validator.Validator;
+import org.apache.any23.validator.ValidatorException;
 import org.apache.any23.writer.JSONLDWriter;
 import org.apache.any23.writer.ReportingTripleHandler;
 import org.apache.any23.writer.TripleHandler;
 import org.apache.any23.writer.TripleHandlerException;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-
 public class TranslatorService {
 
     public static final String NVA_USER_AGENT = "NVA-user-agent";
-    public static final String FAILED_TO_EXTRACT_TRIPLES_FROM_DATA_SOURCE =
-            "Failed to extract triples from data source";
-
+    public static final String FAILED_TO_EXTRACT_TRIPLES_FROM_DOCUMENT = "Failed to extract triples from the document";
+    public static final String ERROR_WHEN_VALIDATING_DOCUMENT = "Error when validating the document";
+    private final Validator validator;
     private ByteArrayOutputStream outputStream;
 
     /**
      * Creates a new translator for HTML metadata to RDF.
-     * @throws IOException On IO exceptions.
      */
-    public TranslatorService() throws IOException {
-
+    public TranslatorService() {
+        validator = new DefaultValidator();
+        validator.addRule(NoLowercaseDcPrefixRule.class, NoLowercaseDcPrefixFix.class);
+        validator.addRule(NoLowercaseDctermsPrefixRule.class, NoLowercaseDctermsPrefixFix.class);
     }
 
     /**
      * Returns a ByteArrayOutputStream of the extracted metadata.
+     *
      * @return ByteArrayOutputStream
      */
     public ByteArrayOutputStream getOutputStream() {
@@ -42,18 +51,23 @@ public class TranslatorService {
 
     /**
      * Dereference and process metadata from URI to RDF.
+     *
      * @param uri URI to be dereferenced.
-     * @throws URISyntaxException If the URI is invalid.
-     * @throws IOException If the IO fails.
+     * @throws URISyntaxException  If the URI is invalid.
+     * @throws IOException         If the IO fails.
      * @throws ExtractionException If the extraction fails.
      */
     public void loadMetadataFromUri(URI uri) throws URISyntaxException, IOException, ExtractionException {
         try (TripleHandler handler = createTripleHandler()) {
             Any23 translator = createAny23();
             DocumentSource source = new HTTPDocumentSource(translator.getHTTPClient(), uri.toString());
+            validator.validate(uri, new TagSoupParser(source.openInputStream(), source.getDocumentIRI()).getDOM(),
+                true);
             translator.extract(source, handler);
         } catch (TripleHandlerException e) {
-            throw new RuntimeException(FAILED_TO_EXTRACT_TRIPLES_FROM_DATA_SOURCE);
+            throw new RuntimeException(FAILED_TO_EXTRACT_TRIPLES_FROM_DOCUMENT);
+        } catch (ValidatorException e) {
+            throw new RuntimeException(ERROR_WHEN_VALIDATING_DOCUMENT);
         }
     }
 
@@ -67,6 +81,6 @@ public class TranslatorService {
         outputStream = new ByteArrayOutputStream();
         JSONLDWriter rdfWriterHandler = new JSONLDWriter(outputStream);
         return new ReportingTripleHandler(
-                new IgnoreAccidentalRDFa(new IgnoreTitlesOfEmptyDocuments(rdfWriterHandler), true));
+            new IgnoreAccidentalRDFa(new IgnoreTitlesOfEmptyDocuments(rdfWriterHandler), true));
     }
 }
