0d77937b35837c25a66e4e09d4e1a40aeaeb73e5
brinxmat <789709+brinxmat@users.noreply.github.com>
2021-03-16 18:07:24 +0100

    Add year-month pattern

--- metadata-transformer/src/test/java/no/unit/nva/metadata/service/testdata/DateArgumentsProvider.java
+++ metadata-transformer/src/test/java/no/unit/nva/metadata/service/testdata/DateArgumentsProvider.java
@@ -12,18 +12,25 @@ public class DateArgumentsProvider implements ArgumentsProvider {
     public Stream<? extends Arguments> provideArguments(ExtensionContext context) {
         return Stream.of(
                 Arguments.of("DC.date", "2001"),
+                Arguments.of("DC.date", "2001-01"),
                 Arguments.of("DC.date", "2001-01-01"),
                 Arguments.of("dc.date", "2001"),
+                Arguments.of("dc.date", "2001-01"),
                 Arguments.of("dc.date", "2001-01-01"),
                 Arguments.of("DCTERMS.date", "2001"),
+                Arguments.of("DCTERMS.date", "2001-01"),
                 Arguments.of("DCTERMS.date", "2001-01-01"),
                 Arguments.of("dcterms.date", "2001"),
+                Arguments.of("dcterms.date", "2001-01"),
                 Arguments.of("dcterms.date", "2001-01-01"),
                 Arguments.of("citation_publication_date", "2001"),
+                Arguments.of("citation_publication_date", "2001-01"),
                 Arguments.of("citation_publication_date", "2001-01-01"),
                 Arguments.of("citation_cover_date", "2001"),
+                Arguments.of("citation_cover_date", "2001-01"),
                 Arguments.of("citation_cover_date", "2001-01-01"),
                 Arguments.of("citation_date", "2001"),
+                Arguments.of("citation_date", "2001-01"),
                 Arguments.of("citation_date", "2001-01-01")
         );
     }
