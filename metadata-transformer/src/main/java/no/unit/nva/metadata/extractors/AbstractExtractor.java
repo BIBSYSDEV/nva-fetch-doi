ba554ccee5cefaf6f27877c527647f7f579b2e58
brinxmat <789709+brinxmat@users.noreply.github.com>
2021-04-04 16:25:52 +0200

    Add suppression of spurious closeResource warning

--- metadata-transformer/src/main/java/no/unit/nva/metadata/extractors/AbstractExtractor.java
+++ metadata-transformer/src/main/java/no/unit/nva/metadata/extractors/AbstractExtractor.java
@@ -18,6 +18,7 @@ public final class AbstractExtractor {
 
     }
 
+    @SuppressWarnings("PMD.CloseResource")
     private static EntityDescription extract(ExtractionPair extractionPair) {
         Statement statement = extractionPair.getStatement();
         EntityDescription entityDescription = extractionPair.getEntityDescription();
