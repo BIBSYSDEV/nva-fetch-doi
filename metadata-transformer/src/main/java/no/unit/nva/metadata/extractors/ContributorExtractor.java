8a34d828775d13b28d14396e03735033699eee8d
brinxmat <789709+brinxmat@users.noreply.github.com>
2021-04-08 16:45:00 +0200

    Remove handling for error that cannot happen

--- metadata-transformer/src/main/java/no/unit/nva/metadata/extractors/ContributorExtractor.java
+++ metadata-transformer/src/main/java/no/unit/nva/metadata/extractors/ContributorExtractor.java
@@ -5,10 +5,7 @@ import no.unit.nva.model.EntityDescription;
 import no.unit.nva.model.Identity;
 import no.unit.nva.model.NameType;
 import no.unit.nva.model.Role;
-import no.unit.nva.model.exceptions.MalformedContributorException;
 import nva.commons.core.JacocoGenerated;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,38 +15,23 @@ import static java.util.Objects.isNull;
 import static nva.commons.core.attempt.Try.attempt;
 
 public final class ContributorExtractor {
-    private static final Logger logger = LoggerFactory.getLogger(ContributorExtractor.class);
 
-    public static final Function<ExtractionPair, EntityDescription> apply = extractOrConsumeError();
+    public static final Function<ExtractionPair, EntityDescription> apply = ContributorExtractor::extract;
 
     @JacocoGenerated
     private ContributorExtractor() {
 
     }
 
-    private static Function<ExtractionPair, EntityDescription> extractOrConsumeError() {
-        return (extractionPair) -> attempt(() -> extract(extractionPair))
-                .orElse(fail -> defaultValue(extractionPair));
-    }
-
-    private static EntityDescription defaultValue(ExtractionPair extractionPair) {
-        logger.warn("Could not create contributor for statement " + extractionPair.getStatement());
-        return extractionPair.getEntityDescription();
-    }
-
-    private static EntityDescription extract(ExtractionPair extractionPair) throws MalformedContributorException {
+    private static EntityDescription extract(ExtractionPair extractionPair) {
         if (extractionPair.isContributor()) {
             addContributor(extractionPair);
         }
         return extractionPair.getEntityDescription();
     }
 
-    private static void addContributor(ExtractionPair extractionPair)
-            throws MalformedContributorException {
-        Contributor contributor = new Contributor.Builder()
-                .withRole(Role.CREATOR)
-                .withIdentity(extractIdentity(extractionPair.getObject()))
-                .build();
+    private static void addContributor(ExtractionPair extractionPair) {
+        Contributor contributor = createContributorWithoutCorrespondingAuthorInfo(extractionPair);
         EntityDescription entityDescription = extractionPair.getEntityDescription();
         List<Contributor> contributorList = entityDescription.getContributors();
         if (isNull(contributorList)) {
@@ -59,6 +41,13 @@ public final class ContributorExtractor {
         } else {
             contributorList.add(contributor);
         }
+    }
+
+    private static Contributor createContributorWithoutCorrespondingAuthorInfo(ExtractionPair extractionPair) {
+        return attempt(() -> new Contributor.Builder()
+                .withRole(Role.CREATOR)
+                .withIdentity(extractIdentity(extractionPair.getObject()))
+                .build()).orElseThrow();
     }
 
     private static Identity extractIdentity(String name) {
