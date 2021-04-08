cbaf115d0cbc96e080a4611a138dc20c067dae5a
brinxmat <789709+brinxmat@users.noreply.github.com>
2021-04-08 08:02:52 +0200

    Refactor filter

--- metadata-transformer/src/main/java/no/unit/nva/metadata/filters/FilterShorterTitles.java
+++ metadata-transformer/src/main/java/no/unit/nva/metadata/filters/FilterShorterTitles.java
@@ -2,30 +2,35 @@ package no.unit.nva.metadata.filters;
 
 import no.unit.nva.metadata.extractors.ExtractorUtil;
 import no.unit.nva.metadata.type.DcTerms;
-import org.eclipse.rdf4j.model.Literal;
 import org.eclipse.rdf4j.model.Model;
 import org.eclipse.rdf4j.model.Statement;
 import org.eclipse.rdf4j.model.Value;
+
+import java.util.Optional;
 
 /**
  * Filters triples with property dcterms:title from the document model by length comparison, keeping the longest title.
  */
 public final class FilterShorterTitles {
     public static boolean apply(Model documentModel, Statement statement) {
-        if (isNotTitle(statement)) {
-            return false;
-        }
-
-        return documentModel.stream().filter(item -> DcTerms.TITLE.getIri().equals(statement.getPredicate()))
-                .map(Statement::getObject)
-                .filter(object -> object instanceof Literal)
-                .map(Value::stringValue)
-                .map(String::length)
-                .anyMatch(length -> length > statement.getObject().stringValue().length());
+        return getTitleLength(documentModel).map(length -> selectLongestTitle(statement, length))
+                .orElse(false);
     }
 
-    private static boolean isNotTitle(Statement statement) {
-        return ExtractorUtil.isNotLiteral(statement.getObject())
-                && !DcTerms.TITLE.getIri().equals(statement.getPredicate());
+    private static Optional<String> getTitleLength(Model documentModel) {
+        return documentModel.stream()
+                .filter(FilterShorterTitles::isTitle)
+                .map(Statement::getObject)
+                .map(Value::stringValue)
+                .findAny();
+    }
+
+    private static boolean selectLongestTitle(Statement statement, String title) {
+        return title.length() > statement.getObject().stringValue().length();
+    }
+
+    private static boolean isTitle(Statement statement) {
+        return DcTerms.TITLE.getIri().equals(statement.getPredicate())
+                && !ExtractorUtil.isNotLiteral(statement.getObject());
     }
 }
