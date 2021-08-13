15c508d8bae776e2a10227659086b0af310879aa
Orestis Gkorgkas <orestis.gkorgkas@unit.no>
2021-08-13 15:16:06 +0200

    NP-2516: convert localDate to Instant in standard way

--- doi-transformer/src/test/java/no/unit/nva/doi/transformer/CrossRefConverterTest.java
+++ doi-transformer/src/test/java/no/unit/nva/doi/transformer/CrossRefConverterTest.java
@@ -27,6 +27,7 @@ import java.net.URI;
 import java.nio.file.Path;
 import java.time.Instant;
 import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
@@ -183,6 +184,20 @@ public class CrossRefConverterTest extends ConversionTest {
     }
 
     @Test
+    public void toPublicationReturnsPublicationWithFilteredOutContributorsLackingBothNameAndSurname() {
+        CrossRefDocument crossRefDocument = sampleJournalArticle();
+
+        int initialContributorsNumber = crossRefDocument.getAuthor().size();
+        List<CrossrefContributor> newContributors = createAuthorListWithAnonymousAuthor(crossRefDocument);
+        crossRefDocument.setAuthor(newContributors);
+        assertThat(crossRefDocument.getAuthor().size(), is(equalTo(initialContributorsNumber + 1)));
+
+        Publication publication = toPublication(crossRefDocument);
+        List<Contributor> actualContributors = publication.getEntityDescription().getContributors();
+        assertThat(actualContributors.size(), is(equalTo(initialContributorsNumber)));
+    }
+
+    @Test
     @DisplayName("An empty CrossRef document throws IllegalArgument exception")
     void anEmptyCrossRefDocumentThrowsIllegalArgumentException() throws IllegalArgumentException {
         CrossRefDocument crossRefDocument = new CrossRefDocument();
@@ -213,30 +228,6 @@ public class CrossRefConverterTest extends ConversionTest {
         toPublication(crossRefDocument);
         String expectedError = String.format(UNRECOGNIZED_TYPE_MESSAGE, "null");
         assertThat(testAppender.getMessages(), containsString(expectedError));
-    }
-
-    @Test
-    public void toPublicationReturnsPublicationWithFilteredOutContributorsLackingBothNameAndSurname(){
-        CrossRefDocument crossRefDocument = sampleJournalArticle();
-
-        int initialContributorsNumber = crossRefDocument.getAuthor().size();
-        List<CrossrefContributor> newContributors = createAuthorListWithAnonymousAuthor(crossRefDocument);
-        crossRefDocument.setAuthor(newContributors);
-        assertThat(crossRefDocument.getAuthor().size(),is(equalTo(initialContributorsNumber+1)));
-
-        Publication publication = toPublication(crossRefDocument);
-        List<Contributor> actualContributors = publication.getEntityDescription().getContributors();
-        assertThat(actualContributors.size(),is(equalTo(initialContributorsNumber)));
-
-    }
-
-    private List<CrossrefContributor> createAuthorListWithAnonymousAuthor(CrossRefDocument crossRefDocument) {
-        List<CrossrefContributor> newContributors = new ArrayList<>(crossRefDocument.getAuthor());
-        CrossrefContributor anonymousContributor = new CrossrefContributor();
-        anonymousContributor.setSequence("5");
-        anonymousContributor.setAffiliation(sampleAffiliation());
-        newContributors.add(anonymousContributor);
-        return newContributors;
     }
 
     @Test
@@ -712,7 +703,16 @@ public class CrossRefConverterTest extends ConversionTest {
 
     private static Instant sampleCrossrefDateAsInstant() {
         LocalDate date = LocalDate.of(EXPECTED_YEAR, EXPECTED_MONTH, EXPECTED_DAY);
-        return Instant.ofEpochSecond(date.toEpochDay() * 86400L);
+        return date.atStartOfDay(ZoneId.systemDefault()).toInstant();
+    }
+
+    private List<CrossrefContributor> createAuthorListWithAnonymousAuthor(CrossRefDocument crossRefDocument) {
+        List<CrossrefContributor> newContributors = new ArrayList<>(crossRefDocument.getAuthor());
+        CrossrefContributor anonymousContributor = new CrossrefContributor();
+        anonymousContributor.setSequence("5");
+        anonymousContributor.setAffiliation(sampleAffiliation());
+        newContributors.add(anonymousContributor);
+        return newContributors;
     }
 
     private CrossRefDocument crossRefDocumentWithInvalidUrl(String invalidUri) {
