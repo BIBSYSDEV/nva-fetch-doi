5d9cc2e93a9143f6edceda7fae2ad2cf223deacd
Gregor Gabriel <gregor.gabriel@unit.no>
2022-01-14 13:33:17 +0100

    orestis session

--- create-publication-from-scopus/src/main/java/no/sikt/nva/scopus/ScopusHandler.java
+++ create-publication-from-scopus/src/main/java/no/sikt/nva/scopus/ScopusHandler.java
@@ -1,6 +1,8 @@
 package no.sikt.nva.scopus;
 
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import com.amazonaws.services.lambda.runtime.events.S3Event;
 import no.unit.nva.metadata.service.MetadataService;
 import no.unit.nva.s3.S3Driver;
 import nva.commons.apigateway.ApiGatewayHandler;
@@ -8,80 +10,52 @@ import nva.commons.apigateway.RequestInfo;
 import nva.commons.apigateway.exceptions.ApiGatewayException;
 import nva.commons.core.Environment;
 import nva.commons.core.JacocoGenerated;
+import nva.commons.core.JsonUtils;
+import nva.commons.core.ioutils.IoUtils;
 import nva.commons.core.paths.UnixPath;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
+import java.io.*;
 
 //Todo: find our own Response object
-public class ScopusHandler extends ApiGatewayHandler<Void, String> {
+public class ScopusHandler implements RequestStreamHandler {
 
     protected static final String BUCKET_NAME = "BUCKET_NAME";
     public static final String FILE_IDENTIFIER = "fileIdentifier";
-    private final transient S3Driver s3Driver;
-    private final transient PublicationConverter publicationConverter;
-    private final transient PublicationPersistenceService publicationPersistenceService;
-    private final transient MetadataService metadataService;
 
     private static final Logger logger = LoggerFactory.getLogger(ScopusHandler.class);
 
+
     @JacocoGenerated
     public ScopusHandler() {
-        this(new Environment());
     }
 
-    @JacocoGenerated
-    public ScopusHandler(Environment environment) {
-        this(new S3Driver(environment.readEnv(BUCKET_NAME)), new PublicationConverter(),
-                new PublicationPersistenceService(), getMetadataService(), environment);
-    }
 
-    /**
-     * Constructor for ScopusHandler.
-     *
-     * @param environment  environment.
-     */
-    public ScopusHandler(S3Driver s3Driver,
-                       PublicationConverter publicationConverter,
-                       PublicationPersistenceService publicationPersistenceService,
-                       MetadataService metadataService,
-                       Environment environment) {
-        super(Void.class, environment);
-        this.s3Driver = s3Driver;
-        this.publicationConverter = publicationConverter;
-        this.publicationPersistenceService = publicationPersistenceService;
-        this.metadataService = metadataService;
-    }
+//    @Override
+//    protected String processInput(Void input, RequestInfo requestInfo, Context context) throws ApiGatewayException {
+//        String filename = requestInfo.getQueryParameter(FILE_IDENTIFIER);
+//        String contents = s3Driver.getFile(UnixPath.fromString(filename));
+//        // parse file to a ScopusPublication
+////              ScopusPublication scopusPublication = publicationConverter.convert(contents);
+//        // enrich contributors with help of nva-cristin-service
+//        // enrich organizations with help of nva-cristin-service
+//        // enrich journal with help of nva-publication-channels
+//        // enrich publisher with help of nva-publication-channels
+//        // metadataTransform ScopusPublication into CreatePublicationRequest
+//        metadataService.toString();
+//        // send CreatePublicationRequest to nva-publication-service
+//        publicationPersistenceService.toString();
+//        return contents;
+//    }
 
-    @JacocoGenerated
-    private static MetadataService getMetadataService() {
-        try {
-            return new MetadataService();
-        } catch (IOException e) {
-            throw new RuntimeException("Error creating handler", e);
+
+    @Override
+    public void handleRequest(InputStream input, OutputStream output, Context context) throws IOException {
+        String streamString = IoUtils.streamToString(input);
+        S3Event event = JsonUtils.dtoObjectMapper.readValue(streamString, S3Event.class);
+        try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(output))) {
+            writer.write(event.getRecords().get(0).getS3().getObject().getKey());
         }
-    }
-
-    @Override
-    protected String processInput(Void input, RequestInfo requestInfo, Context context) throws ApiGatewayException {
-        String filename = requestInfo.getQueryParameter(FILE_IDENTIFIER);
-        String contents = s3Driver.getFile(UnixPath.fromString(filename));
-        // parse file to a ScopusPublication
-//              ScopusPublication scopusPublication = publicationConverter.convert(contents);
-        // enrich contributors with help of nva-cristin-service
-        // enrich organizations with help of nva-cristin-service
-        // enrich journal with help of nva-publication-channels
-        // enrich publisher with help of nva-publication-channels
-        // metadataTransform ScopusPublication into CreatePublicationRequest
-        metadataService.toString();
-        // send CreatePublicationRequest to nva-publication-service
-        publicationPersistenceService.toString();
-        return null;
-    }
-
-    @Override
-    protected Integer getSuccessStatusCode(Void input, String output) {
-        return null;
     }
 }
--- create-publication-from-scopus/src/test/java/no/sikt/nva/scopus/ScopusHandlerTest.java
+++ create-publication-from-scopus/src/test/java/no/sikt/nva/scopus/ScopusHandlerTest.java
@@ -4,12 +4,14 @@ import com.amazonaws.services.lambda.runtime.CognitoIdentity;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.net.HttpHeaders;
+import com.mchange.v2.c3p0.util.TestUtils;
 import no.unit.nva.api.CreatePublicationRequest;
 import no.unit.nva.metadata.service.MetadataService;
 import no.unit.nva.model.EntityDescription;
 import no.unit.nva.s3.S3Driver;
 import no.unit.nva.stubs.FakeContext;
 import no.unit.nva.testutils.HandlerRequestBuilder;
+import no.unit.nva.testutils.IoUtils;
 import nva.commons.apigateway.ApiGatewayHandler;
 import nva.commons.apigateway.GatewayResponse;
 import nva.commons.core.Environment;
@@ -19,6 +21,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
@@ -28,10 +31,12 @@ import static org.apache.http.HttpHeaders.CONTENT_TYPE;
 import static org.apache.http.HttpStatus.SC_BAD_REQUEST;
 import static org.apache.http.HttpStatus.SC_INTERNAL_SERVER_ERROR;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.hasKey;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.contains;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -41,6 +46,8 @@ class ScopusHandlerTest {
     private Environment environment;
     private Context context;
     public static final ObjectMapper restServiceObjectMapper = JsonUtils.dtoObjectMapper;
+    private ScopusHandler handler;
+    private ByteArrayOutputStream outputStream;
 
     /**
      * Set up environment.
@@ -49,56 +56,17 @@ class ScopusHandlerTest {
     public void setUp() {
         environment = mock(Environment.class);
         context = new FakeContext();
+        handler = new ScopusHandler();
+        outputStream = new ByteArrayOutputStream();
         when(environment.readEnv(ApiGatewayHandler.ALLOWED_ORIGIN_ENV)).thenReturn(ALL_ORIGINS);
+        when(environment.readEnv(ScopusHandler.BUCKET_NAME)).thenReturn("bucketName");
     }
 
     @Test
-    public void testResponseWithEmptyRequest()
-            throws Exception {
-        HandlerRequestBuilder<Map<String, String>> handlerRequestBuilder = new HandlerRequestBuilder<>(restServiceObjectMapper);
-        String generatedString = RandomStringUtils.randomAlphanumeric(10);
-        Map<String, String> queryParams = new HashMap<>();
-        queryParams.put(ScopusHandler.FILE_IDENTIFIER, generatedString);
-        handlerRequestBuilder.withQueryParameters(queryParams);
-        InputStream inputStream = handlerRequestBuilder.build();
-        ScopusHandler handler = createHandler();
-        ByteArrayOutputStream output = new ByteArrayOutputStream();
-        handler.handleRequest(inputStream, output, context);
-        GatewayResponse<String> gatewayResponse = GatewayResponse.fromOutputStream(output);
-        assertEquals(SC_INTERNAL_SERVER_ERROR, gatewayResponse.getStatusCode());
-        assertThat(gatewayResponse.getHeaders(), hasKey(CONTENT_TYPE));
-        assertThat(gatewayResponse.getHeaders(), hasKey(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN));
-        assertNull(handler.getSuccessStatusCode(null, null));
-    }
-
-    private ScopusHandler createHandler() {
-        S3Driver s3Client = mock(S3Driver.class);
-        PublicationConverter publicationConverter = mock(PublicationConverter.class);
-        PublicationPersistenceService publicationPersistenceService = mock(PublicationPersistenceService.class);
-        MetadataService metadataService = mockMetadataServiceReturningSuccessfulResult();
-
-        return new ScopusHandler(s3Client, publicationConverter, publicationPersistenceService, metadataService,
-                environment);
-    }
-
-    private MetadataService mockMetadataServiceReturningSuccessfulResult() {
-        MetadataService service = mock(MetadataService.class);
-
-        EntityDescription entityDescription = new EntityDescription();
-        entityDescription.setMainTitle("Main title");
-        CreatePublicationRequest request = new CreatePublicationRequest();
-        request.setEntityDescription(entityDescription);
-
-        when(service.generateCreatePublicationRequest(any())).thenReturn(Optional.of(request));
-        return service;
-    }
-
-    private Context getMockContext() {
-        Context context = mock(Context.class);
-        CognitoIdentity cognitoIdentity = mock(CognitoIdentity.class);
-        when(context.getIdentity()).thenReturn(cognitoIdentity);
-        when(cognitoIdentity.getIdentityPoolId()).thenReturn("junit");
-        return context;
+    public  void shouldReturnUriWhenInputIsS3Event() throws IOException {
+        InputStream event = IoUtils.inputStreamFromResources("event.json");
+        handler.handleRequest(event, outputStream, context);
+        assertThat(outputStream.toString(), containsString("orestisUri"));
     }
 
 
