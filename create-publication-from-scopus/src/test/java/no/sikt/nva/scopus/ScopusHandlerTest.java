bccd935d572e157f519f2a4bc063d11d0fbf0c22
Gregor Gabriel <gregor.gabriel@unit.no>
2022-01-14 15:05:36 +0100

    scopusHandler aware of s3Events

--- create-publication-from-scopus/src/test/java/no/sikt/nva/scopus/ScopusHandlerTest.java
+++ create-publication-from-scopus/src/test/java/no/sikt/nva/scopus/ScopusHandlerTest.java
@@ -1,73 +1,9 @@
 package no.sikt.nva.scopus;
 
-import com.amazonaws.services.lambda.runtime.CognitoIdentity;
-import com.amazonaws.services.lambda.runtime.Context;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.common.net.HttpHeaders;
-import com.mchange.v2.c3p0.util.TestUtils;
-import no.unit.nva.api.CreatePublicationRequest;
-import no.unit.nva.metadata.service.MetadataService;
-import no.unit.nva.model.EntityDescription;
-import no.unit.nva.s3.S3Driver;
-import no.unit.nva.stubs.FakeContext;
-import no.unit.nva.testutils.HandlerRequestBuilder;
-import no.unit.nva.testutils.IoUtils;
-import nva.commons.apigateway.ApiGatewayHandler;
-import nva.commons.apigateway.GatewayResponse;
-import nva.commons.core.Environment;
-import nva.commons.core.JsonUtils;
-import org.apache.commons.lang3.RandomStringUtils;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
-
-import static org.apache.http.HttpHeaders.CONTENT_TYPE;
-import static org.apache.http.HttpStatus.SC_BAD_REQUEST;
-import static org.apache.http.HttpStatus.SC_INTERNAL_SERVER_ERROR;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.hasKey;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.contains;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
 class ScopusHandlerTest {
 
-    public static final String ALL_ORIGINS = "*";
-    private Environment environment;
-    private Context context;
-    public static final ObjectMapper restServiceObjectMapper = JsonUtils.dtoObjectMapper;
-    private ScopusHandler handler;
-    private ByteArrayOutputStream outputStream;
 
-    /**
-     * Set up environment.
-     */
-    @BeforeEach
-    public void setUp() {
-        environment = mock(Environment.class);
-        context = new FakeContext();
-        handler = new ScopusHandler();
-        outputStream = new ByteArrayOutputStream();
-        when(environment.readEnv(ApiGatewayHandler.ALLOWED_ORIGIN_ENV)).thenReturn(ALL_ORIGINS);
-        when(environment.readEnv(ScopusHandler.BUCKET_NAME)).thenReturn("bucketName");
-    }
 
-    @Test
-    public  void shouldReturnUriWhenInputIsS3Event() throws IOException {
-        InputStream event = IoUtils.inputStreamFromResources("event.json");
-        handler.handleRequest(event, outputStream, context);
-        assertThat(outputStream.toString(), containsString("orestisUri"));
-    }
 
 
 }
