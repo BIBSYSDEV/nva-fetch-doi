723d1a5f8168660a6dcb0cc02f04b56df29cfb0d
Orestis Gkorgkas <orestis.gkorgkas@unit.no>
2022-01-19 14:19:30 +0100

    SMILE-1239: fix create publication request

--- metadata-transformer/src/main/java/no/unit/nva/metadata/CreatePublicationRequest.java
+++ metadata-transformer/src/main/java/no/unit/nva/metadata/CreatePublicationRequest.java
@@ -5,12 +5,10 @@ import com.fasterxml.jackson.databind.JsonNode;
 import java.net.URI;
 import java.util.List;
 import java.util.Objects;
-import java.util.Set;
 import no.unit.nva.WithContext;
 import no.unit.nva.WithFile;
 import no.unit.nva.WithMetadata;
 import no.unit.nva.file.model.FileSet;
-import no.unit.nva.model.AdditionalIdentifier;
 import no.unit.nva.model.EntityDescription;
 import no.unit.nva.model.ResearchProject;
 import nva.commons.core.JacocoGenerated;
@@ -23,8 +21,6 @@ public class CreatePublicationRequest implements WithMetadata, WithFile, WithCon
     private JsonNode context;
     private List<ResearchProject> projects;
     private List<URI> subjects;
-    private Set<AdditionalIdentifier> additionalIdentifiers;
-
 
     @JacocoGenerated
     @Override
@@ -85,7 +81,6 @@ public class CreatePublicationRequest implements WithMetadata, WithFile, WithCon
     public void setContext(JsonNode context) {
         this.context = context;
     }
-
 
     @JacocoGenerated
     @Override
