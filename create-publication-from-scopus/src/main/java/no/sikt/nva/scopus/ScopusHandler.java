6e2b04796ca4d9482f8a015b23a255f423742bdb
anetteOlli <ansi@unit.no>
2022-01-21 08:56:51 +0100

    SMILE-1239-extract-scopus-id, removed unused import

--- create-publication-from-scopus/src/main/java/no/sikt/nva/scopus/ScopusHandler.java
+++ create-publication-from-scopus/src/main/java/no/sikt/nva/scopus/ScopusHandler.java
@@ -17,7 +17,6 @@ import software.amazon.awssdk.services.s3.S3Client;
 
 import java.io.StringReader;
 import java.net.URI;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
