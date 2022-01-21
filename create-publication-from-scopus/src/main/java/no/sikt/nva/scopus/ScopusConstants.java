1a979f59814485967f7d963bcd73430aef6db884
anetteOlli <ansi@unit.no>
2022-01-21 11:14:35 +0100

    SMILE-1239-extract-scopus-id, code-review

--- create-publication-from-scopus/src/main/java/no/sikt/nva/scopus/ScopusConstants.java
+++ create-publication-from-scopus/src/main/java/no/sikt/nva/scopus/ScopusConstants.java
@@ -4,7 +4,7 @@ import nva.commons.core.JacocoGenerated;
 
 public class ScopusConstants {
     //identifier fieldsnames:
-    public static final String ADDITIONAL_IDENTIFIERS_SCOPUS_ID_SOURCE_NAME = "ScopusId";
+    public static final String ADDITIONAL_IDENTIFIERS_SCOPUS_ID_SOURCE_NAME = "scopusIdentifier";
     public static final String SCOPUS_ITEM_IDENTIFIER_SCP_FIELD_NAME = "scp";
 
     @JacocoGenerated
