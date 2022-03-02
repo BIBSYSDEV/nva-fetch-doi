cd02d8235a2c186735b9e5beb217fa22e15fa088
nr <nr@unit.no>
2022-03-02 14:34:34 +0100

    Removed unnecessary use of scopus-xml-file in test

--- create-publication-from-scopus/src/test/java/no/sikt/nva/scopus/ScopusHandlerTest.java
+++ create-publication-from-scopus/src/test/java/no/sikt/nva/scopus/ScopusHandlerTest.java
@@ -151,10 +151,6 @@ class ScopusHandlerTest {
     private static final String PUBLICATION_YEAR_FIELD_NAME = "year";
     private static final String FILENAME_EXPECTED_ABSTRACT_IN_0000469852 = "expectedAbstract.txt";
     private static final String EXPECTED_ABSTRACT_NAME_SPACE = "<abstractTp";
-    private static final String EXPECTED_ISSUE_IN_0000469852 = "1";
-    private static final String EXPECTED_VOLUME_IN_0000469852 = "50";
-    private static final String EXPECTED_FIRST_PAGE_IN_0000469852 = "105";
-    private static final String EXPECTED_LAST_PAGE_IN_0000469852 = "119";
     private static final String JOURNAL_SOURCETYPE_IDENTIFYING_CHAR = "j";
     private static final String BOOK_SOURCETYPE_IDENTIFYING_CHAR = "b";
 
@@ -626,16 +622,19 @@ class ScopusHandlerTest {
 
     @Test
     void shouldExtractVolumeIssueAndPageRange() throws IOException {
-        var scopusFile = IoUtils.stringFromResources(Path.of(SCOPUS_XML_0000469852));
-        var uri = s3Driver.insertFile(randomS3Path(), scopusFile);
+        scopusData = ScopusGenerator.create(CitationtypeAtt.AR);
+        var expectedIssue = String.valueOf(randomInteger());
+        var expectedVolume = randomString();
+        var expectedPages = randomString();
+        scopusData.setJournalInfo(expectedVolume, expectedIssue, expectedPages);
+        var uri = s3Driver.insertFile(UnixPath.of(randomString()), scopusData.toXml());
         var s3Event = createS3Event(uri);
         var createPublicationRequest = scopusHandler.handleRequest(s3Event, CONTEXT);
         var actualPublicationInstance = (JournalArticle) createPublicationRequest.getEntityDescription().getReference()
                 .getPublicationInstance();
-        assertThat(actualPublicationInstance.getVolume(), is(EXPECTED_VOLUME_IN_0000469852));
-        assertThat(actualPublicationInstance.getIssue(), is(EXPECTED_ISSUE_IN_0000469852));
-        assertThat(actualPublicationInstance.getPages().getBegin(), is(EXPECTED_FIRST_PAGE_IN_0000469852));
-        assertThat(actualPublicationInstance.getPages().getEnd(), is(EXPECTED_LAST_PAGE_IN_0000469852));
+        assertThat(actualPublicationInstance.getVolume(), is(expectedVolume));
+        assertThat(actualPublicationInstance.getIssue(), is(expectedIssue));
+        assertThat(actualPublicationInstance.getPages().getEnd(), is(expectedPages));
     }
 
     @Test
