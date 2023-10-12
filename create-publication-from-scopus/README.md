c9b1e4883be45ef845294cc78ab57c2ffa1a3ba0
anetteOlli <anette.olli.siiri@sikt.no>
2023-10-12 14:59:00 +0200

    bump-nva-datamodel, removing traces of scopus-import from this repository

--- create-publication-from-scopus/README.md
+++
@@ -1,3 +0,0 @@
-# CREATE PUBLICATION FROM SCOPUS
-
-Project that converts scopus xml to NVA publication.
--- create-publication-from-scopus/src/main/java/no/sikt/nva/scopus/exception/UnsupportedSrcTypeException.java
+++
@@ -1,8 +0,0 @@
-package no.sikt.nva.scopus.exception;
-
-public class UnsupportedSrcTypeException extends RuntimeException {
-
-    public UnsupportedSrcTypeException(String message) {
-        super(message);
-    }
-}
